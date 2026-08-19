7faab59b67dcba9daf262c7593abaf49ba21865b
Hanze Chen <hanze2010@outlook.com>
2026-08-19 13:19:49 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,6 @@
       </main>
       <div v-else id="loading">
         <img src="/icon/logo.png" alt="logo" class="logo" />
-        <span class="tip">开发中</span>
       </div>
     </Transition>
   </Provider>
